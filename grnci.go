3d32cc66a4a18b01feb327f43d10b3529761afe6
Susumu Yata <susumu.yata@gmail.com>
2016-01-08 16:55:03 +0900

    Add Time.MarshalJSON() and Geo.MarshalJSON()
    GitHub: #13

--- grnci.go
+++ grnci.go
@@ -760,6 +760,16 @@ func (time Time) Unix() (sec, nsec int64) {
 	return
 }
 
+// MarshalJSON() returns a JSON-encoded value.
+func (val Time) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprint(float64(int64(val))/1000000.0)), nil
+}
+
+// MarshalJSON() returns a JSON-encoded value.
+func (val Geo) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf("\"%d,%d\"", val.Lat, val.Long)), nil
+}
+
 //
 // `table_create`
 //
