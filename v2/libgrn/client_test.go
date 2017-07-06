9d654943e08513d715e23d2e0c838ddb2c90b363
Susumu Yata <susumu.yata@gmail.com>
2017-07-06 16:34:01 +0900

    Disable annoying tests.

--- v2/libgrn/client_test.go
+++ v2/libgrn/client_test.go
@@ -1,10 +1,6 @@
 package libgrn
 
 import (
-	"io"
-	"io/ioutil"
-	"log"
-	"strings"
 	"testing"
 
 	"github.com/groonga/grnci/v2"
@@ -59,54 +55,54 @@ import (
 // 	}
 // }
 
-func TestClientDB(t *testing.T) {
-	type Pair struct {
-		Command string
-		Body    string
-	}
-	pairs := []Pair{
-		// Pair{"no_such_command", ""},
-		Pair{"status", ""},
-		Pair{`table_create Tbl TABLE_PAT_KEY ShortText`, ""},
-		Pair{`column_create Tbl Col COLUMN_SCALAR Int32`, ""},
-		Pair{`load --table Tbl --values '[["_key"],["test"]]'`, ""},
-		Pair{`load --table Tbl --values '[["_key"],["test" invalid_format]]'`, ""},
-		Pair{"load --table Tbl", `[["_key"],["test"]]`},
-		Pair{"load --table Tbl", `[["_key"],["test" invalid_format]]`},
-		Pair{"select --table Tbl", ""},
-		Pair{"dump", ""},
-	}
+// func TestClientDB(t *testing.T) {
+// 	type Pair struct {
+// 		Command string
+// 		Body    string
+// 	}
+// 	pairs := []Pair{
+// 		// Pair{"no_such_command", ""},
+// 		Pair{"status", ""},
+// 		Pair{`table_create Tbl TABLE_PAT_KEY ShortText`, ""},
+// 		Pair{`column_create Tbl Col COLUMN_SCALAR Int32`, ""},
+// 		Pair{`load --table Tbl --values '[["_key"],["test"]]'`, ""},
+// 		Pair{`load --table Tbl --values '[["_key"],["test" invalid_format]]'`, ""},
+// 		Pair{"load --table Tbl", `[["_key"],["test"]]`},
+// 		Pair{"load --table Tbl", `[["_key"],["test" invalid_format]]`},
+// 		Pair{"select --table Tbl", ""},
+// 		Pair{"dump", ""},
+// 	}
 
-	client, err := OpenClient("/tmp/db/db")
-	if err != nil {
-		t.Skipf("OpenClient failed: %v", err)
-	}
-	defer client.Close()
+// 	client, err := OpenClient("/tmp/db/db")
+// 	if err != nil {
+// 		t.Skipf("OpenClient failed: %v", err)
+// 	}
+// 	defer client.Close()
 
-	for _, pair := range pairs {
-		var body io.Reader
-		if pair.Body != "" {
-			body = strings.NewReader(pair.Body)
-		}
-		log.Printf("command = %s", pair.Command)
-		resp, err := client.Exec(pair.Command, body)
-		if err != nil {
-			t.Fatalf("client.Exec failed: %v", err)
-		}
-		result, err := ioutil.ReadAll(resp)
-		if err != nil {
-			t.Fatalf("ioutil.ReadAll failed: %v", err)
-		}
-		log.Printf("start = %v, elapsed = %v", resp.Start(), resp.Elapsed())
-		log.Printf("result = %s", result)
-		if err := resp.Err(); err != nil {
-			log.Printf("err = %v", err)
-		}
-		if err := resp.Close(); err != nil {
-			t.Fatalf("resp.Close failed: %v", err)
-		}
-	}
-}
+// 	for _, pair := range pairs {
+// 		var body io.Reader
+// 		if pair.Body != "" {
+// 			body = strings.NewReader(pair.Body)
+// 		}
+// 		log.Printf("command = %s", pair.Command)
+// 		resp, err := client.Exec(pair.Command, body)
+// 		if err != nil {
+// 			t.Fatalf("client.Exec failed: %v", err)
+// 		}
+// 		result, err := ioutil.ReadAll(resp)
+// 		if err != nil {
+// 			t.Fatalf("ioutil.ReadAll failed: %v", err)
+// 		}
+// 		log.Printf("start = %v, elapsed = %v", resp.Start(), resp.Elapsed())
+// 		log.Printf("result = %s", result)
+// 		if err := resp.Err(); err != nil {
+// 			log.Printf("err = %v", err)
+// 		}
+// 		if err := resp.Close(); err != nil {
+// 			t.Fatalf("resp.Close failed: %v", err)
+// 		}
+// 	}
+// }
 
 func TestClientHandler(t *testing.T) {
 	var i interface{} = &Client{}
