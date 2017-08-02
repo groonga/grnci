22fb497b34f01bb9394f8c8dc94a5ff478d5dfac
Susumu Yata <susumu.yata@gmail.com>
2017-08-02 12:21:41 +0900

    Add a test for ErrorCode.

--- v2/error_test.go
+++ v2/error_test.go
@@ -1,8 +1,35 @@
 package grnci
 
 import (
+	"encoding/json"
 	"testing"
 )
+
+func TestErrorCode(t *testing.T) {
+	data, err := json.Marshal(AddressError)
+	if err != nil {
+		t.Fatalf("json.Marshal failed: %v", err)
+	}
+	if want := `"AddressError"`; string(data) != want {
+		t.Fatalf("json.Marshal failed: actual = %s, want = %s", data, want)
+	}
+
+	data, err = json.Marshal(ErrorCode(-22))
+	if err != nil {
+		t.Fatalf("json.Marshal failed: %v", err)
+	}
+	if want := `"GRN_INVALID_ARGUMENT"`; string(data) != want {
+		t.Fatalf("json.Marshal failed: actual = %s, want = %s", data, want)
+	}
+
+	data, err = json.Marshal(ErrorCode(-12345))
+	if err != nil {
+		t.Fatalf("json.Marshal failed: %v", err)
+	}
+	if want := "-12345"; string(data) != want {
+		t.Fatalf("json.Marshal failed: actual = %s, want = %s", data, want)
+	}
+}
 
 func TestNewError(t *testing.T) {
 	data := map[string]interface{}{
