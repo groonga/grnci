1cd54eba5d89ce78f43345cf9000420940c00e83
Susumu Yata <susumu.yata@gmail.com>
2017-12-06 18:25:56 +0900

    Update a test for Log

--- v2/libgrn/libgrn_test.go
+++ v2/libgrn/libgrn_test.go
@@ -1,22 +1,20 @@
 package libgrn
 
 import (
-	"io/ioutil"
 	"os"
 	"path/filepath"
 	"testing"
 )
 
 func TestLog(t *testing.T) {
-	dir, err := ioutil.TempDir("", "libgrn")
-	if err != nil {
-		t.Fatalf("ioutil.TempDir failed: %v", err)
-	}
-	defer os.RemoveAll(dir)
+	db, dir := makeDB(t)
+	defer removeDB(db, dir)
+
 	path := filepath.Join(dir, "groonga.log")
 	Log(path, nil)
-	Init()
-	Fin()
+	if err := db.TableCreate("Tbl", nil); err != nil {
+		t.Fatalf("db.Status failed: %v", err)
+	}
 	if _, err := os.Stat(path); err != nil {
 		t.Fatalf("os.Stat failed: %v", err)
 	}
@@ -25,6 +23,7 @@ func TestLog(t *testing.T) {
 func TestQueryLog(t *testing.T) {
 	db, dir := makeDB(t)
 	defer removeDB(db, dir)
+
 	path := filepath.Join(dir, "query.log")
 	QueryLog(path, nil)
 	if _, err := db.Status(); err != nil {
